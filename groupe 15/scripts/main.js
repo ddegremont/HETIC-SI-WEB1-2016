aaf288a687feb0ff0c697c6c38affb14a93b221d
zelazna <constantin.guidon@gmail.com>
2016-05-20 00:04:19 +0200

    refonte Yakarouler

---
+++ groupe 15/scripts/main.js
@@ -0,0 +1,199 @@
+$(document).ready(function () {
+
+    // Script du menu / carroussel
+
+    $('.owl-carousel').owlCarousel({
+        responsiveClass:true,
+        responsive: {
+            0: {
+                items: 4
+            },
+            600: {
+                items: 5
+            },
+            800:{
+                items: 6
+            },
+            1000: {
+                items: 11
+            }
+        }
+    });
+
+    //autocompletion JQUERY UI
+
+    var availableTags = [
+        'ABS',
+        'abs',
+        'accélérateur',
+        'accessoire',
+        'accroche',
+        'accumulateur',
+        'admission',
+        'aile',
+        'ailette',
+        'airbag',
+        'alimentation',
+        'allumage',
+        'allume-cigare',
+        'allumeur',
+        'alternateur',
+        'amortisseur',
+        'ampoule',
+        'antenne',
+        'anti-vol',
+        'antiblocage de sécurité',
+        'antibrouillard',
+        'antidémarrage',
+        'antidémarreur',
+        'antigel',
+        'antipatinage',
+        'antiplongée',
+        'antivol',
+        'arbre à cames',
+        'autoradio',
+        'banquette',
+        'baquet',
+        'batterie',
+        'bielle',
+        'biellette',
+        'bloc-cylindres',
+        'bloc-moteur',
+        'bobine d’allumage',
+        'boîte à gants',
+        'boîte automatique',
+        'boite de vitesse',
+        'boîte de vitesse automatique',
+        'boite de vitesse manuelle',
+        'bougie',
+        'bougie d’allumage',
+        'bougie de préchauffage',
+        'calandre',
+        'capot',
+        'capote',
+        'carburateur',
+        'cardan',
+        'carrosserie',
+        'carter',
+        'chaîne',
+        'chambre à air',
+        'châssis',
+        'clé à bougies',
+        'clé de contact',
+        'clignotant',
+        'climatisation',
+        'compte-tour',
+        'compteur de vitesse',
+        'courroie de transmission',
+        'coussinet',
+        'crémaillère',
+        'cric',
+        'culasse',
+        'culbuteur',
+        'déflecteur',
+        'dégivrage',
+        'démonte-pneu',
+        'différentiel',
+        'direction',
+        'dispositif antidémarrage',
+        'disque',
+        'embrayage',
+        'enjoliveur',
+        'essieu',
+        'essieu arrière',
+        'essieu avant',
+        'essuie-glace',
+        'essuie-phare',
+        'feu',
+        'filtre',
+        'frein',
+        'frein à main',
+        'hayon',
+        'immatriculation',
+        'injecteur',
+        'jante',
+        'joint',
+        'joint de culasse',
+        'joint torique',
+        'jupe avant',
+        'lave-glace',
+        'lève-vitre',
+        'levier de vitesse',
+        'marchepied',
+        'pare-brise',
+        'pare-choc',
+        'parebrise',
+        'pédale',
+        'phare',
+        'pignon',
+        'piston',
+        'plaque d’immatriculation',
+        'pneu',
+        'pompe à eau',
+        'pompe à huile',
+        'pompe auxiliaire',
+        'portière',
+        'pot catalytique',
+        'pot d’échappement',
+        'radiateur',
+        'rétroviseur',
+        'rotule',
+        'roue',
+        'roue de secours',
+        'siège',
+        'siège arrière',
+        'siège avant',
+        'soupape d’admission',
+        'soupape d’échappement',
+        'transmission',
+        'transmission automatique',
+        'transmission manuelle',
+        'turbo',
+        'turbo-compresseur',
+        'turbocompresseur',
+        'valve',
+        'vidange',
+        'vilebrequin',
+        'virage',
+        'volant'
+    ];
+    $("#tags").autocomplete({
+        source: availableTags,
+        autoFocus: true,
+        minLength: 2
+    });
+
+    // Effect Ripple sur les bouttons
+//@fixme probleme du positionnement absolu
+    $(".button").mouseenter(function (e) {
+        var parentOffset = $(this).offset(),
+            cursorX = e.pageX - parentOffset.left,
+            cursorY = e.pageY - parentOffset.top;
+        $(this).children(".ripple").remove();
+        $(this).append("<div class=\"ripple\"</div>");
+        $(this).children(".ripple").css({"left": cursorX + "px", "top": cursorY + "px"})
+    });
+
+    //Effet Switch de formulaires
+    var init = function () {
+        var carousel = document.getElementById('carousel'),
+            navButtons = document.querySelectorAll('#navigation button'),
+            panelCount = carousel.children.length,
+            transformProp = Modernizr.prefixed('transform'),
+            theta = 0,
+
+            onNavButtonClick = function (event) {
+                var increment = parseInt(event.target.getAttribute('data-increment'));
+                theta += ( 180 / panelCount ) * increment * -1;
+                carousel.style[transformProp] = 'translateZ( -288px ) rotateY(' + theta + 'deg)';
+            };
+
+        for (var i = 0; i < 2; i++) {
+            navButtons[i].addEventListener('click', onNavButtonClick, false);
+        }
+
+    };
+    window.addEventListener('DOMContentLoaded', init, false);
+});
+
+
